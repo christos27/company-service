e722542f1ebc8f7d6f9e96d801da24a5a1b8fb2d
christos27 <1429824+christos27@users.noreply.github.com>
2025-02-23 14:20:07 +0200

    Set up postgres db

---
+++ db/initdb.d/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS companies (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(15) UNIQUE NOT NULL,
+    description TEXT,
+    amount_of_employees INT NOT NULL,
+    registered BOOLEAN NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('Corporations', 'NonProfit', 'Cooperative', 'Sole Proprietorship'))
+);
